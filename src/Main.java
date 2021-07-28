605ff67d953e8ccb409741d7def1ba56d72dcdf1
Swirly <tian.heng@students.mq.edu.au>
2021-07-28 15:51:05 +1000

    Week 1 Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
+
+    // Q1. Bitbucket and GitHub are softwares for version control. They provide
+    // users with ways to save their work onto a cloud platform. I personally think
+    // GitHub is better than Bitbucket, but this is only because I've worked with
+    // GitHub before in the past and am used to it.
+
+    // Q2. The last commit for the Bitbucket project was done by Matthew Roberts,
+    // on the 2021/07/09.
+
+    // Q3. The first commit for the Bitbucket project was done by Jon Mountjoy,
+    // on the 2014/08/11.
+
+    // Q4. The last commit for the Github project was done by Dave Pearce, on the
+    // 2021/07/25. The first commit for the GitHub project was done by Timothy Jones,
+    // on the 2011/15/11
+
+    // Q5. The Bitbucket project is currently not active. While there are recent
+    // commits by Matthew Roberts, the commits are just him fiddling around.
+    // This may be because the project was completed. The GitHub project is 
+    // currently active however.
+
+    // Q6. For the GitHub project, the "pom.xml" file is probably the most active file,
+    // as it seems to be a very important file in the application. For the
+    // Bitbucket project, maybe the "qualtrics.csv" file is the most active,
+    // as it is the biggest file in the project. 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World");
+        System.out.println("~~~~~");
+        System.out.println("Red vs. Blue");
+        System.out.println("~~~~~");
     }
 }
